5798d2051ae6565f79df000bdedeffe0797437f7
[Nikita15503] <[pvrprvng@gmail.com]>
2025-06-02 20:08:54 +0300

    test Nikita

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
+      <h1>Привет</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
