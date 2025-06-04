cd78e131f23afb30ef45acc0edd6823935c06123
Evangeline <evangelinaersova02@gmail.com>
2025-06-05 01:06:36 +0300

    catalog layout

---
+++ client/src/components/Catalog.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+function Catalog() {
+  return (
+    <div> <p1 className='catalog-heading'>Каталог</p1>
+  
+      <div className='catalog-options'>
+        
+        <div className='accessories'>
+          <div className='yellow-acc'></div>
+        </div>
+
+        <div className='middle-container'>
+
+          <div className='envelopes'><div className='yellow-env'></div></div>
+          <div className='clothes'><div className='yellow-min'></div></div>
+          <div className='nappies'><div className='yellow-min'></div></div>
+          
+
+        </div>
+
+        <div className='setlists'><div className='yellow-set'></div></div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Catalog
