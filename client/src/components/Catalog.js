cfefb1f636ffb053f48642aca4dfcf83197d79e8
Evangeline <evangelinaersova02@gmail.com>
2025-06-05 22:07:17 +0300

    new catalog layour

--- client/src/components/Catalog.js
+++ client/src/components/Catalog.js
@@ -1,26 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 function Catalog() {
+
+  let[optionsOpen, setOptionsOpen]=useState(false)
+
   return (
     <div> <p1 className='catalog-heading'>Каталог</p1>
   
       <div className='catalog-options'>
         
         <div className='accessories'>
-          <div className='yellow-acc'></div>
+          <div className='yellow-acc'>
+            <p className='group-name'>АКСЕССУАРЫ</p><div className='triangle'></div>
+          </div>
         </div>
 
         <div className='middle-container'>
 
-          <div className='envelopes'><div className='yellow-env'></div></div>
-          <div className='clothes'><div className='yellow-min'></div></div>
-          <div className='nappies'><div className='yellow-min'></div></div>
+          <div className='envelopes'>
+            <div className='yellow-env'>
+              <p className='group-name'>КОНВЕРТЫ И ПЛЕДЫ</p><div className='triangle'> </div>
+            </div>
+          </div>
+
+          <div className='clothes'>
+            <div className='yellow-min' onClick={()=>setOptionsOpen(optionsOpen=!optionsOpen)} >
+
+              {optionsOpen && (
+                <div className='clothes-panel'></div>
+              )}
+
+              <p className='group-name'>ОДЕЖДА</p><div className='triangle'> </div>
+            </div>
+          </div>
+
+          <div className='nappies'>
+            <div className='yellow-min'>
+              <p className='group-name'>ПЕЛЕНКИ</p><div className='triangle'> </div>
+            </div>
+          </div>
           
 
         </div>
 
-        <div className='setlists'><div className='yellow-set'></div></div>
+        <div className='setlists'>
+          <div className='yellow-set'>
+            <p className='group-name'>ГОТОВЫЕ ПОДБОРКИ</p><div className='triangle'> </div>
+          </div>
+        </div>
 
       </div>
     </div>
