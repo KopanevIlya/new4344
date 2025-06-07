1f11bf2f3b5f892faab35b5efda3720b82b34e3f
1dayzy1 <dayzy173@gmail.com>
2025-06-07 19:01:57 +0300

    Cart

---
+++ client/src/components/Cart.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import './Cart.css'
+
+function Cart() {
+  return (
+    <div className='containerCatalog'>
+
+    <div className='cart'>
+
+        <div className='img'><img src='/logo.png' /></div>
+        
+        <p className='title'>Подборка №106</p>
+
+    </div>
+
+
+    <div className='cart'>
+
+        <div className='img'><img src='' /></div>
+        
+        <p className='title'>Подборка №106</p>
+
+    </div>
+
+
+    <div className='cart'>
+
+            <div className='img'><img src='' /></div>
+
+            <p className='title'>Подборка №106</p>
+
+            </div>
+
+
+            <div className='cart'>
+
+            <div className='img'><img src='' /></div>
+
+            <p className='title'>Подборка №106</p>
+
+            </div>
+
+            <div className='cart'>
+
+            <div className='img'><img src='' /></div>
+
+            <p className='title'>Подборка №106</p>
+
+            </div>
+
+
+    
+      
+    </div>
+  )
+}
+
+export default Cart
