d63838db3394b8af1589a965452c580e847b120a
KopanevIlya <ilya.kopanev@mail.ru>
2025-06-07 18:56:11 +0300

    change Poster

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Header from './components/Header';
+import Poster from './components/Poster'
 
 function App() {
   return (
     <div className="App">
       
       <Header />
+      <Poster />
 
       
     </div>
