e5b50b4c9556e0fac1bf07bf9ad51b5998f469a4
KopanevIlya <ilya.kopanev@mail.ru>
2025-06-07 19:40:00 +0300

    change Pocter

--- client/src/components/Poster.js
+++ client/src/components/Poster.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import './Poster.css'
 
 function Poster() {
   return (
-    <div>
-      gfdjngfkjx  
+    <div className='wrapper_poster'>
+      <div className='container-poster'>
+        <div className='left-img_poster'>
+          <h1>Большая зимняя <br></br> распродажа от LeoKid</h1>
+          <p>Конверт Leokid Classic подходит для использования <br></br> в любых колясках, автокреслах и санках. Для <br></br> прогулок осенью, зимой, весной и даже <br></br> прохладным летом.</p>
+        </div>
+        <div className='right-img-poster'></div>
+        
+      </div> 
     </div>
   )
 }
