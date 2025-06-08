cd3fdab93a367cf75c615d31912a6eb26a7eb9a7
Evangeline <evangelinaersova02@gmail.com>
2025-06-08 09:56:56 +0300

    about text

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Header from './components/Header';
 import Poster from './components/Poster'
+import About from './components/About';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header />
 
       <Poster />
+      <About />
 
 
       
