7a844fb875a061f3d1106f91d7735d597b42d711
Evangeline <evangelinaersova02@gmail.com>
2025-06-09 20:03:59 +0300

    images added

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
 
 import './App.css';
 import Header from './components/Header';
-import Poster from './components/Poster'
-import About from './components/About';
+import Cart from './components/Cart';
+import Poster from './components/Poster';
+import Catalog from './components/Catalog';
+import Modal from './components/Modal';
+import Subscribe from './components/Subscribe'
+
 
 
 function App() {
   return (
     <div className="App">
-      
-      <Header />
-
+      <Header/>
       <Poster />
-      <About />
-
-
-      
+      <Catalog/>
+      <Cart/>
+      <Modal/>
+      <Subscribe/>
     </div>
   );
 }
