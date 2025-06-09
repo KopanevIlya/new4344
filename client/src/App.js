0884cb9604dec45769dc6c4898235ed3307896eb
KopanevIlya <ilya.kopanev@mail.ru>
2025-06-09 19:50:40 +0300

    change 09_06 new components and backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,22 @@
 import './App.css';
 import Header from './components/Header';
-
 import Cart from './components/Cart';
-
 import Poster from './components/Poster';
 import Catalog from './components/Catalog';
-
 import Modal from './components/Modal';
+import Subscribe from './components/Subscribe'
+
 
 
 function App() {
   return (
     <div className="App">
-      
-
       <Header/>
-     
-
-      
-
-      
-
       <Poster />
-
       <Catalog/>
-      
-
-
       <Cart/>
-
-
       <Modal/>
-
-
-
+      <Subscribe/>
     </div>
   );
 }
