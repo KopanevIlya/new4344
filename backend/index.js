9bdd830ea9034e4d689a361c6cf35206f65a12fe
[Nikita15503] <[pvrprvng@gmail.com]>
2025-06-28 20:28:52 +0300

    Nikita

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"123Qwe45 ",
+    password:"funtik03",
     database:"lala_store"
 })
 
